1062951b789430a237f0ecd4882db437160cc31b
wsmachado1845-netizen <wsmachado1845@gmail.com>
2026-09-01 06:29:31 -0300

    v8.0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // EducaPlan — Service Worker
 // Cache simples do "app shell" para permitir abrir o app mesmo sem internet.
-const CACHE_NAME = "educaplan-cache-v63";
+const CACHE_NAME = "educaplan-cache-v64";
 const APP_SHELL = [
   "./",
   "./index.html",
